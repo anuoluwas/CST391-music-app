79f715a2adb534bfb711e5a017b34185168615b7
AnuoluwaSajo <asajo@gcu.edu>
2026-02-20 23:13:30 -0700

    Connect to deployed PostgreSQL musicApp DB in vercel

--- pages/api/db-check.ts
+++ pages/api/db-check.ts
@@ -7,11 +7,14 @@ const dbURL = process.env.DATABASE_URL;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try{
         const db = getPool();
-        const {rows} = await db.query('select now() as now');
-        res.status(200).json({time: rows[0], message: `Sparks Database connection successfully. Running in ${environment}.DATABASE_URL: ${dbURL}`});
+        let {rows} = await db.query('select now() as now');
+        const now = rows[0]?.now;
+        ({rows} = await db.query('SELECT artist FROM albums LIMIT 1'))
+        const artist = rows[0]?.artist;
+        res.status(200).json({time: now, artist: artist, message: `Anuoluwa Database connection successfully. Running in ${environment}.DATABASE_URL: ${dbURL}`});
     }
     catch (err) {
         res.status(500).json({error: 'Database connection failed', details: (err as Error).message,
-            message:`Sparks database connection failed. Running in ${environment}.DATABASE_URL: ${dbURL}`})
+            message:`Anuoluwa database connection failed. Running in ${environment}.DATABASE_URL: ${dbURL}`})
     }
 }
