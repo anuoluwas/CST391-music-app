07d7469b1c858d0c568c576a9895d516f5112631
AnuoluwaSajo <asajo@gcu.edu>
2026-04-05 22:15:08 -0700

    Added nav bar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Link from 'next/link';
+import { useEffect } from 'react';
+export default function NavBar() {
+    useEffect(() => {
+        // @ts-expect-error: Bootstrap's JavaScript bundle lacks TypeScript definitions.
+        import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }, []);
+
+    return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <span className="navbar-brand">My Music</span>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+                <Link href="/public" className="nav-item nav-link">
+                    Main
+                </Link>
+                <Link href="/new" className="nav-item nav-link">
+                    New
+                </Link>
+            </div>
+        </div>
+    </nav>);
+}
