080e83abf886f0f3ff0973cf9ca8c4584f475247
AnuoluwaSajo <asajo@gcu.edu>
2026-04-19 23:20:57 -0700

    Updated auth for edit page

--- app/playlists/edit/[playlistId]/page.tsx
+++ app/playlists/edit/[playlistId]/page.tsx
@@ -22,7 +22,7 @@ export default function EditPlaylistPage() {
     const {data: session} = useSession()
 
     useEffect(() => {
-        if (session) {
+        if (!session) {
             router.push("/playlist")
         }
     }, [session]);
